87658cf3fd8b3dc7aaa4846962294a429cdd0e89
Akshay@2003 <akshay88magar@gmail.com>
2023-04-07 01:04:44 +0530

    IF location is On then Accessing Permission Is checked by using Dexter

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -9,6 +9,11 @@ import android.os.Bundle
 import android.provider.Settings
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
@@ -24,10 +29,48 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
         else{
-            Toast.makeText(this,"Your location Provider is Already ON",Toast.LENGTH_LONG).show()
+            Dexter.withActivity(this)
+                .withPermissions(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        if (report!!.areAllPermissionsGranted()) {
+                            // TODO (STEP 7: Call the location request function here.)
+                            // START
+                            requestLocationData()
+                            // END
+                        }
+
+                        if (report.isAnyPermissionPermanentlyDenied) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "You have denied location permission. Please allow it is mandatory.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+                }).onSameThread()
+                .check()
         }
 
 
+    }
+
+    private fun showRationalDialogForPermissions() {
+        TODO("Not yet implemented")
+    }
+
+    private fun requestLocationData() {
+        TODO("Not yet implemented")
     }
 
     private fun isLocationEnabled():Boolean{
