3632d1a9bade5e9b1abebe2ad6c26cff9065ae12
Akshay@2003 <akshay88magar@gmail.com>
2023-04-06 23:54:51 +0530

    Location ON or NOT is checked.

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.weatherapp
 
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        if(!isLocationEnabled()){
+            Toast.makeText(this,"Your location Provider is turn off  Please turn it on",Toast.LENGTH_LONG).show()
+        }
+        else{
+            Toast.makeText(this,"Your location Provider is Already ON",Toast.LENGTH_LONG).show()
+        }
+
+
+    }
+
+    private fun isLocationEnabled():Boolean{
+        //This provide access to the system loaction services
+        val locationManager: LocationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+       return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 }
